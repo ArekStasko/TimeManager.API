8092c5fe75eb64b51a35555111cea1b6f4382ab1
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-09-01 08:56:55 +0200

    Add processors tests templates

---
+++ TimeManager.API.Tests/ActivityProcTests.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using FluentAssertions;
+using TimeManager.API.Data;
+using TimeManager.API.Processors.vwActivityCategoryProcessor;
+
+namespace TimeManager.API.Tests
+{
+    public class ActivityProcTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ActivityAdd_Should_AddActivity()
+        {
+            var processor = ActivityProcessor_Factory.GetActivity_Add();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ActivityDelete_Should_DeleteActivity()
+        {
+            var processor = ActivityProcessor_Factory.GetActivity_Delete();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ActivityGetAll_Should_ReturnAllActivities()
+        {
+            var processor = ActivityProcessor_Factory.GetvwActivityCategory_GetAll();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ActivityGetByCategory_Should_ReturnActivitiesByCategory()
+        {
+            var processor = ActivityProcessor_Factory.GetvwActivityCategory_GetByCategory();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ActivityById_Should_ReturnActivityWithSpecificId()
+        {
+            var processor = ActivityProcessor_Factory.GetvwActivityCategory_GetById();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ActivityUpdate_Should_UpdateActivity()
+        {
+            var processor = ActivityProcessor_Factory.GetActivity_Update();
+            Assert.Pass();
+        }
+    }
+}
---
+++ TimeManager.API.Tests/CategoryProcTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using TimeManager.API.Data;
+using TimeManager.API.Processors.CategoryProcessor;
+
+namespace TimeManager.API.Tests
+{
+    public class CategoryProcTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CategoryAdd_Should_AddCategory()
+        {
+            var processor = CategoryProcessor_Factory.GetCategory_Add();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void CategoryDelete_Should_DeleteCategory()
+        {
+            var processor = CategoryProcessor_Factory.GetCategory_Delete();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void CategoryGet_Should_ReturnCategory()
+        {
+            var processor = CategoryProcessor_Factory.GetCategory_Get();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void CategoryUpdate_Should_UpdateCategory()
+        {
+            var processor = CategoryProcessor_Factory.GetCategory_Update();
+            Assert.Pass();
+        }
+    }
+}
