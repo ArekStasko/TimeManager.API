6c132cd2f0565ec316c85519e24aaf9af51fedf9
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-09-07 17:25:50 +0200

    Change serilog configuration

--- TimeManager.API/Program.cs
+++ TimeManager.API/Program.cs
@@ -5,9 +5,17 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
+// Add services to the container.
 
-    builder.Services.AddControllers();
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,12 +26,6 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
-builder.Host.UseSerilog((ctx, lc) =>
-{
-    lc.WriteTo.Console()
-      .WriteTo.File("timemanagerAPI-.txt", rollingInterval: RollingInterval.Day);
-});
 
 var app = builder.Build();
 
